208529b65b23d51c156c40201a5257ad970bde2d
ShafinAhmad <sahmad7@buffalo.edu>
2025-12-08 09:14:09 -0500

    updated definition signatures to ensure compatibility

--- src/load.py
+++ src/load.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-def load(input: pd.DataFrame) -> None:
+def load(input: pd.DataFrame, rejected: pd.DataFrame) -> None:
     pass
--- src/validate.py
+++ src/validate.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-def validate(input: pd.DataFrame) -> pd.DataFrame:
+def validate(input: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     pass
