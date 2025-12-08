3bfda1547b56db00ea4bd3cf21406868342e34ca
ShafinAhmad <sahmad7@buffalo.edu>
2025-12-08 09:21:00 -0500

    Wrote what I want these two functions to do

--- src/clean.py
+++ src/clean.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 def clean(input: pd.DataFrame) -> pd.DataFrame:
     # [Poster_Link, Series_Title, Released_Year, Certificate, Runtime, Genre, IMDB_Rating, Meta_Score, Director, Star1, Star2, Star3, Star4, No_of_Voters, Gross, Number_of_Movies]
-    # Reject if anything but Poster_link, Certificate, Number_of_Movies is empty
+    # Save everything but Poster_link, Certificate, Number_of_Movies, and Overview
+    # Convert everything to the proper format 
+    # Turn all nulls in box office to 0
+    print(input.head(1))
     pass
--- src/validate.py
+++ src/validate.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 def validate(input: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    # [Poster_Link, Series_Title, Released_Year, Certificate, Runtime, Genre, IMDB_Rating, Meta_Score, Director, Star1, Star2, Star3, Star4, No_of_Voters, Gross, Number_of_Movies]
+    # Reject if anything but Poster_link, Certificate, Number_of_Movies is empty
+    # Reject if movie name is duplicated
     pass
