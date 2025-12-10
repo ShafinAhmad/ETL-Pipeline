9a90d1f8709e3256e847a7efc7090d64cc37d6b1
ShafinAhmad <sahmad7@buffalo.edu>
2025-12-10 10:46:46 -0500

    Added debugging prints to main, remove from final

--- src/main.py
+++ src/main.py
@@ -3,24 +3,32 @@ from load import load
 from clean import clean
 from validate import validate
 from transform import transform
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 
 root_dir = Path(__file__).resolve().parent.parent
 
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
 def display(input: pd.DataFrame) -> None:
-    pass
+    return
 
 def main():
     dataDirectory: Path = root_dir / "data" / "imdb.csv"
     data: pd.DataFrame = read_csv(dataDirectory.resolve())
     # print(data)
     validatedData, rejectedData = validate(data)
+    print("Validated Data")
     cleanedData = clean(validatedData)
+    print("Cleaned Data")
+    # print(cleanedData)
     finalData = transform(cleanedData)
+    print("Transformed Data")
+    # print(finalData)
+    print(finalData[["Series_Title", "Gross", "Gross_Inflation_Adjusted"]])
     load(finalData, rejectedData)
     display(finalData)
-    pass
 
 if __name__=="__main__":
     main()
