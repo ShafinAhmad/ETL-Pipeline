1264e6086fae5d815f6869560cc5e67fbf568665
Shafin Ahmad <701ahmads@gmail.com>
2025-12-18 19:02:18 -0500

    Converted csv_reader to pyspark

--- src/readers/csv_reader.py
+++ src/readers/csv_reader.py
@@ -1,7 +1,9 @@
-import pandas as pd
 from pathlib import Path
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame as SparkDataFrame
 
-def read_csv(fileName: Path) -> pd.DataFrame:
-    data: pd.DataFrame = pd.read_csv(fileName)
+def read_csv(fileName: Path, spark_session: SparkSession) -> SparkDataFrame:
     # [Poster_Link, Series_Title, Released_Year, Certificate, Runtime, Genre, IMDB_Rating, Meta_Score, Director, Star1, Star2, Star3, Star4, No_of_Voters, Gross, Number_of_Movies]
-    return data
+    result = spark_session.read.csv(fileName.absolute().as_posix(), header=True, inferSchema=True)
+    print(result)
+    return result
