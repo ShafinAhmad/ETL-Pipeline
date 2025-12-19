5dad3526fcdeaad6ca74f48ceaed5e0b86b261bd
Shafin Ahmad <701ahmads@gmail.com>
2025-12-18 20:12:36 -0500

    Clean now uses sparkdataframe

--- src/clean.py
+++ src/clean.py
@@ -1,13 +1,9 @@
 import pandas as pd
 from logger import logger
+from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.sql.functions import regexp_replace
 
-def clean_runtime(runtime: str) -> int:
-    return int(runtime.replace("min", "").strip())
-def clean_gross(gross: str) -> int:
-    return int(gross.replace(",", "").strip())
-
-
-def clean(input: pd.DataFrame) -> pd.DataFrame:
+def clean(input: SparkDataFrame) -> SparkDataFrame:
     # [Poster_Link, Series_Title, Released_Year, Certificate, Runtime, Genre, IMDB_Rating, Overview, Meta_score, Director, Star1, Star2, Star3, Star4, No_of_Votes, Gross, Number_of_Movies]
     # Save everything but Poster_link, Certificate, Number_of_Movies, and Overview
     # Convert everything to the proper format
@@ -15,20 +11,19 @@ def clean(input: pd.DataFrame) -> pd.DataFrame:
     # [String, Int, Int, String, Float, Int, String, String, String, String, String, Int, Int]
     # Turn all nulls in box office to 0
     
-    df: pd.DataFrame = input.copy()
-    df = df.drop(columns=["Poster_Link", "Certificate", "Overview", "Number_of_Movies"])
-    df["Released_Year"] = df["Released_Year"].astype(int)
-    df["Runtime"] = df["Runtime"].apply(clean_runtime).astype(int)
-    df["IMDB_Rating"] = df["IMDB_Rating"].astype(float)
-    df["Meta_score"] = df["Meta_score"].astype(int)
-    df["No_of_Votes"] = df["No_of_Votes"].astype(int)
-    df["Gross"] = df["Gross"].apply(clean_gross).astype(int)
+    df = input.drop(*["Poster_Link", "Certificate", "Overview", "Number_of_Movies"])
+    df.toPandas().to_csv("cleaned_input.csv")
+    df = df.withColumn("Released_Year", df["Released_Year"].cast("int"))
+    df = df.withColumn("Runtime", (regexp_replace(df["Runtime"], "min", "")).cast("int"))
+    df = df.withColumn("IMDB_Rating", df["IMDB_Rating"].cast("float"))
+    df = df.withColumn("Meta_score", df["Meta_score"].cast("int"))
+    df = df.withColumn("No_of_Votes", df["No_of_Votes"].cast("int"))
+    df = df.withColumn("Gross", (regexp_replace(df["Gross"], ",", "")).cast("int"))
     
     string_columns = ["Series_Title", "Director", "Star1", "Star2", "Star3", "Star4"]
     for col in string_columns:
-        df[col] = df[col].astype(str).str.strip()
-    
-    df["Genre"] = df["Genre"].str.split(", ")
-    
-    logger.info(f"clean.completed rows={len(df)}")
+        df = df.withColumn(col, df[col].cast("string"))
+        
+    logger.info(f"clean.completed rows={df.count()}")
+    # df.toPandas().to_csv("cleaned_output.csv")
     return df
